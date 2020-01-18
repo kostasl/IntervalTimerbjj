7217a745821886a704825280f34fec8b3dec182c
Kostas Lagogiannis <costaslag@gmail.com>
2020-01-18 18:30:39 +0000

    Added Eaed Easter Egg Matt, improve sound sync

--- timerGui.py
+++ timerGui.py
@@ -15,10 +15,27 @@ global iRounds ##Number of Roll Rounds
 global troundTime ##Duration of each round
 global trestTime ##Duration of each round
 global sndParrol,sndCombat
+global lblEasterEgg
 
 iRounds = 0 ##Count the number of rounds passed
-troundTime = 1 ##min
-trestTime = 0.5
+troundTime = 0.3 ##min
+trestTime = 0.21
+
+def showEasterEgg():
+	global lblEasterEgg
+	#lblRound.pack_forget()
+	if (not lblEasterEgg.winfo_ismapped() ):
+		lblEasterEgg.pack()
+		lblEasterEgg.place(relx=0.2, rely=0.15, anchor=CENTER)
+
+def hideEasterEgg():
+	global lblEasterEgg
+	lblEasterEgg.pack_forget() ##Hide it
+
+def showRound(iRounds):
+	lblRound.pack()
+	lblRound.place(relx=0.8, rely=0.1, anchor=CENTER)
+	txtRound.set('Round {:2}'.format(iRounds) )
 
 def formatTimerString(remainder):
 	return( '{:02}:{:02}'.format(int(remainder.total_seconds()/60), int(remainder.total_seconds())) )
@@ -35,11 +52,14 @@ def show_Resttime(endTime):
 	remainder = endTime - datetime.now()
     
 	##play Time Approach beep
+	#if (remainder.total_seconds()  10 ):
+		
 	if (remainder.total_seconds() <= 10 ):
 		sndBeep.play()
+		showEasterEgg()
 	
 	# remove the microseconds part
-	remainder = remainder - timedelta(microseconds=remainder.microseconds)
+	#remainder = remainder - timedelta(microseconds=remainder.microseconds)
     # Show the time left on  the global label object
 	txt.set(formatTimerString(remainder))
 
@@ -50,7 +70,8 @@ def show_Resttime(endTime):
 		sndCombat.play()
 		endTime = datetime.now() + timedelta(minutes=troundTime)
 		iRounds = iRounds + 1 ##Increment Number of Rounds
-		txtRound.set('Round {:2}'.format(iRounds) )
+		#txtRound.set('Round {:2}'.format(iRounds) )
+		showRound(iRounds)
 		root.after(1000, show_Roundtime,endTime)
 
 
@@ -58,22 +79,23 @@ def show_Roundtime(endTime):
 	##Set State Aesthetics
 	root.configure(background='black')
 	lbl.config(background="black",foreground="#81ced4")
+	hideEasterEgg()
+	lblEasterEgg.pack_forget() ##Hide it
 	
-
 	# Get the time remaining until the event
-	remainder = endTime - datetime.now()
+	remainder = endTime - datetime.now() + timedelta(seconds=1)
 
     # remove the microseconds part
 	remainder = remainder - timedelta(microseconds=remainder.microseconds)
-	
+
 	##play Time Approach beep
 	if (remainder.total_seconds() <= 10 ):
 		sndBeep.play()
-	
+
 	# Show the time left on  the global label object
 	txt.set(formatTimerString(remainder))
-	print(remainder.total_seconds())
-
+	
+	#print(remainder.total_seconds())
 
 	if (remainder.total_seconds() > 1):
 		# Trigger the countdown after 1000ms
@@ -87,6 +109,7 @@ def show_Roundtime(endTime):
 
 
 pygame.init()
+## Load Resources 
 sndBeep = pygame.mixer.Sound("res/beep.wav")
 sndParrol = pygame.mixer.Sound("res/parol.wav")
 sndCombat = pygame.mixer.Sound("res/combats.wav")
@@ -95,11 +118,13 @@ sndCombat = pygame.mixer.Sound("res/combats.wav")
 # Use tkinter lib for showing the clock
 root = Tk()
 root.attributes("-fullscreen", True)
-
 root.bind("x", quit)
 
-# Set the end date and time for the countdown
-endTime = datetime.now() + timedelta(minutes=troundTime)
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+imgEasterEgg  = ImageTk.PhotoImage(Image.open("res/mattbatten.jpeg") )
+#ImageTk.resize((450, 332), Image.ANTIALIAS) 
+imglogo = ImageTk.PhotoImage(Image.open("res/neonLogo.jpeg") )
+
 
 ##Aesthetics 
 fnt = font.Font(family='Verdana', size=80, weight='bold')
@@ -109,15 +134,22 @@ lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 txtRound = StringVar()
 lblRound = ttk.Label(root, textvariable=txtRound, font=fnt, foreground="#81ced4", background="black")
-lblRound.place(relx=0.8, rely=0.1, anchor=CENTER)
 
-#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-imglogo = ImageTk.PhotoImage(Image.open("res/neonLogo.jpeg") )
+##Add Logo To Center top
 lbllogo = ttk.Label(root, image=imglogo).pack(side="top")
 
+lblEasterEgg = ttk.Label(root, image=imgEasterEgg) ##.pack(side="top")
+lblEasterEgg.pack_forget() ##Hide it
 ##Start The Round Timer recursive 
 iRounds = iRounds + 1
-txtRound.set('Round {:2}'.format(iRounds) )
-root.after(0, show_Roundtime,endTime)
+
+surface = pygame.Surface((800,600))
+pygame.draw.circle(surface,pygame.Color(250, 250, 250,200),(300, 60), 50, 10)
+
+# Set the end date and time for the countdown
+endTime = datetime.now() + timedelta(minutes=troundTime,seconds=1)
+showRound(iRounds)
+show_Roundtime(endTime)
+#root.after(0, show_Roundtime,)
 
 root.mainloop()
