dae5ffcc8322f3689efb73c7a1f52fa20655cd1b
kostas lagogiannis <costaslag@gmail.com>
2020-01-20 22:21:57 +0000

    Added BG Animation Of Rest

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # IntervalTimerbjj
-An interval timer used for time keeping the roll rounds in bjj - 
+In an attempt to contribute to the martial arts gym opened by my friends here in Southampton, UK, I made this simple interval timer that we can use for time keeping the roll rounds in Brazilian Jiu-Jitsu.
+
+It is designed to run on a Raspberry Pi Zero connected to a display with sound. 
+To avoid the need to use a mouse and keyboard or touchscreen,  the timer reads input from a  button connected  (through pull_up) to GPIO pin 21. 
+The  main idea was to keep this simple so anyone can use who is running the class. In this spirit this is  timer has a single button that is used to start and stop/reset.
+ 
+The timer has two countdown states, going from active (fight round) countdown, and then to a rest period countdown before the next round
+countdown initiates. It keeps track of the number of cycles/Rounds and displays it on the main interface. It makes a warning beeps during the last 10sec before countdown in either state is due.
+     
+
+This version has been customized my for my gym, Neon Martial Arts, showing its logo through the playback of voices for Parol (End the fight)
+and "Combats".
+ It also allows of cheeky hidden (easteregg) picture of one of your training buddies to appear when its time to select a new partnet.
+
+I am new to python and thus this little fun project has helped me get more acquainted with this scripting language.  
+I used tkinter for interface design, and pygame for sound playback.
+
+##Issues:
+ Pygame  appears to not be very reliable on the pi Zero and sounds breakup or do not synchronize.
+
+##Oss!
+### Konstantinos Lagogiannis 2020
+
+
+
--- timerGui.py
+++ timerGui.py
@@ -16,6 +16,8 @@ from tkinter import ttk
 from tkinter import font
 from PIL import ImageTk, Image ##For Jpeg
 
+import colorsys as colsys
+
 from datetime import datetime,timedelta,time
 
 
@@ -52,6 +54,12 @@ if RPI_PLATFORM:
 	GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO21
 	bState = ButtonState(GPIO.input(21))
 
+# convert colour from integer tuple to Hex Value string
+def _from_rgb(rgb):
+    #"""translates an rgb tuple of int to a tkinter friendly color code
+    #"""
+    return "#%02x%02x%02x" % rgb
+
 
 def quit(*args):
 	root.destroy()
@@ -80,7 +88,7 @@ def InputToggle(args):
 	print("Key Pressed")
 	if (cState == TimerState.STOPPED):
 		startTimer()
-	elif (cState == TimerState.ROLL):
+	elif (cState == TimerState.ROLL or cState == TimerState.REST):
 		stopTimer()
 
 
@@ -129,53 +137,79 @@ def showRound(iRounds):
 def formatTimerString(remainder):
 	return( '{:02}:{:02}'.format(int(remainder.total_seconds()/60), int(remainder.total_seconds())) )
 
-def show_Resttime(endTime): 
-    global iRounds
-    ##Set State Aesthetics
-    root.configure(background='blue')
-    lbl.config(background="blue",foreground="black")
-    
-    if (cState == TimerState.STOPPED):
-        return(0)
+def bgColourChange(col):
+	root.configure(background=_from_rgb(col))
+	lbl.config(background=_from_rgb (col),foreground="black")
+	lblRound.config(background=_from_rgb (col),foreground="black")
 
-    # Get the time remaining until the event
-    remainder = endTime - datetime.now()
+# Recursive Call to go up down the blue Colours to show rest breathing effect
+def bgColourAnimate(i,dir):
+	##Animate BG Colour - Show Breathing Rythm
+	t = 0
+	#for col in (col_blue):
+	#root.after( (200*len(col_blue) + 200*(t-len(col_blue) )  ), bgColourChange, col)
+	t += 1
+	##iF sTILL in REST sTATE tHEN REPEAT Colour Cycle
+	#if (cState == TimerState.REST):
 
-    ##play Time Approach beep
-    #if (remainder.total_seconds()  10 ):
-            
-    if (remainder.total_seconds() <= 10 ):
-        sndBeep.play()
-        showEasterEgg()
+	##Ladder Up/Down The idx
+	if (i == (len(col_blue)-1) ):
+		dir=-1 ##Down
+	elif (i == 0):
+		dir=1 ##Up Again
+
+	print(i)
+	bgColourChange( col_blue[i])
+	i=dir+i
+	if (cState == TimerState.REST):
+		root.after(300, bgColourAnimate, i,dir)
+
+
+def show_Resttime(endTime):
+	global iRounds,cState
+
+	#	pygame.time.delay(200)
+
+	if (cState == TimerState.STOPPED):
+		return(0)
+
+	# Get the time remaining until the event
+	remainder = endTime - datetime.now()
+
+	##play Time Approach beep
+	if (remainder.total_seconds() <= 10 ):
+		sndBeep.play()
+		showEasterEgg()
     
     # remove the microseconds part
     #remainder = remainder - timedelta(microseconds=remainder.microseconds)
 # Show the time left on  the global label object
-    txt.set(formatTimerString(remainder))
+	txt.set(formatTimerString(remainder))
 
-    if (remainder.total_seconds() > 1):
-            # Carry Rest CountDown If Timer Is not Stopped
-            if (cState != TimerState.STOPPED):
-                    root.after(1000, show_Resttime,endTime)
-    else:
-            sndCombat.play()
-            endTime = datetime.now() + timedelta(minutes=troundTime)
-            iRounds = iRounds + 1 ##Increment Number of Rounds
+	if (remainder.total_seconds() > 1):
+			# Carry Rest CountDown If Timer Is not Stopped
+			if (cState != TimerState.STOPPED):
+				root.after(1000, show_Resttime,endTime)
+	else:
+			sndCombat.play()
+			endTime = datetime.now() + timedelta(minutes=troundTime)
+			iRounds = iRounds + 1 ##Increment Number of Rounds
             #txtRound.set('Round {:2}'.format(iRounds) )
-            showRound(iRounds)
-            hideEasterEgg()
-            if (cState != TimerState.STOPPED):
-                    root.after(1000, show_Roundtime,endTime)
+			showRound(iRounds)
+			hideEasterEgg()
+			if (cState != TimerState.STOPPED):
+				cState = TimerState.ROLL
+				root.after(1000, show_Roundtime,endTime)
 
 
 def show_Roundtime(endTime):
-	##Set State Aesthetics
+	global cState
+	##Set State Aesthetics / Black BG
 	root.configure(background='black')
 	lbl.config(background="black",foreground="#81ced4")
 	
 	if (cState == TimerState.STOPPED):
 		return(0)
-	
 
 	# Get the time remaining until the event
 	remainder = endTime - datetime.now() + timedelta(seconds=1)
@@ -200,6 +234,8 @@ def show_Roundtime(endTime):
 		# Set the end date and time for the countdown
 		endTime = datetime.now() + timedelta(minutes=trestTime)
 		sndParrol.play()
+		cState = TimerState.REST # Change Time Stater To REST
+		bgColourAnimate(0, 1)
 		root.after(1000, show_Resttime,endTime)
 
 
@@ -231,9 +267,18 @@ fnts = font.Font(family='Verdana', size=60, weight='bold')
 txt = StringVar()
 lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="#81ced4", background="black")
 lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+#hsv_to_rgb(h, s, v)    Convert the color from HSV coordinates to RGB coordinates.
+
+## Make Array of Colours For The Breathing/Rest Animation
+i=0
+col_blue = []
+while  (i < 11):
+	col_blue.append( tuple(round(i * 255) for i in colsys.hsv_to_rgb(205/360,0.4 + 0.6*i/10,1) ) )
+	#print( col_blue[i] )
+	i += 1
 
 txtRound = StringVar()
-lblRound = ttk.Label(root, textvariable=txtRound, font=fnts, foreground="#81ced4", background="black")
+lblRound = ttk.Label(root, textvariable=txtRound, font=fnts, foreground="#81ced4" , background="black")
 
 ##Add Logo To Center top
 lbllogo = ttk.Label(root, image=imglogo).pack(side="top")
@@ -244,7 +289,8 @@ lblEasterEgg.pack_forget() ##Hide it
 iRounds = iRounds + 1
 
 surface = pygame.Surface((800,600))
-pygame.draw.circle(surface,pygame.Color(250, 250, 250,200),(300, 60), 50, 10)
+#pygame.Color(250, 250, 250,200)
+pygame.draw.circle(surface,col_blue[1],(300, 60), 50, 10)
 
 #root.after(0, show_Roundtime,)
 
