13aa51f177f3e1f65f0ae80ea01681c549b87f37
Kostas Lagogiannis <costaslag@gmail.com>
2020-02-15 23:52:02 +0000

    Adafruit Sensor Read

--- DHT_Sensor.py
+++ DHT_Sensor.py
@@ -7,7 +7,7 @@ dhtDevice = adafruit_dht.DHT22(board.D7)
 
 while True:
     try:
->>>>>>> e8caa60224879da76ff149a09bef1f9d1edd1867
+
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
@@ -19,8 +19,4 @@ while True:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
 
-<<<<<<< HEAD
     time.sleep(2.0)
-=======
-    time.sleep(2.0)
->>>>>>> e8caa60224879da76ff149a09bef1f9d1edd1867
